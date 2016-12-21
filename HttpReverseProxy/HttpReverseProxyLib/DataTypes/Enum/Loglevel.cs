b24353020d00696302406678f30b4649a3588cca
Ruben Unteregger <ruben.unteregger@zhdk.ch>
2016-12-21 08:36:33 +0100

    Add loglevel command line parameter

---
+++ HttpReverseProxy/HttpReverseProxyLib/DataTypes/Enum/Loglevel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HttpReverseProxyLib.DataTypes.Enum
+{
+  using System;
+
+
+  [Flags]
+  public enum Loglevel : int
+  {
+    DEBUG = 1,
+    INFO = 2,
+    WARNING = 3,
+    ERROR = 4
+  }
+}
