a0885062acd692a6bfe5ab36b6851ab47edc948b
Ruben Unteregger <ruben.unteregger@gmail.com>
2019-03-20 07:38:29 +0100

    Fix plugin config file path bug

--- HttpReverseProxy/RequestRedirect/Config.cs
+++ HttpReverseProxy/RequestRedirect/Config.cs
@@ -18,9 +18,9 @@
 
     public static int PluginPriority { get; private set; } = 3;
 
-    public static string ConfigFileName { get; private set; } = "0.1";
+    public static string ConfigFileName { get; private set; } = "plugin.config";
 
-    public static string PluginVersion { get; private set; } = "plugin.config";
+    public static string PluginVersion { get; private set; } = "0.1";
 
     public static List<RequestRedirectConfigRecord> RequestRedirectRecords { get; set; } = new List<RequestRedirectConfigRecord>();
 
